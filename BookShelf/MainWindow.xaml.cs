d84f20ff3dca4e3edb2bf9daf312801f863ccd5c
Vlad Gutkovsky <vladgutkovskii@gmail.com>
2017-12-11 16:06:28 +0300

    Hotkeys introduction

--- BookShelf/MainWindow.xaml.cs
+++ BookShelf/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Collections.ObjectModel;
 using Microsoft.Win32;
 using BookShelf.PluginSystem;
 using BookShelf.Pages;
+using System.Configuration;
 
 namespace BookShelf
 {
@@ -34,7 +35,6 @@ namespace BookShelf
             //BookLoader = new PageLoader(AppCur.LibraryData.Books.ToList<object>(), typeof(BookOverview));
             //AuthorLoader = new PageLoader(AppCur.LibraryData.Authors.ToList<object>(), typeof(AuthorOverview));
             //PublisherLoader = new PageLoader(AppCur.LibraryData.Publishers.ToList<object>(), typeof(PublisherOverview));
-            
         }
 
         // Delete Book
@@ -146,7 +146,7 @@ namespace BookShelf
                 AuthorView.Visibility = Visibility.Visible;
         }
 
-        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        private void LoadFile()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
@@ -166,7 +166,12 @@ namespace BookShelf
             }
         }
 
-        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            LoadFile();
+        }
+
+        private void SaveFile()
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             if (saveFileDialog.ShowDialog() == true)
@@ -182,6 +187,11 @@ namespace BookShelf
                         MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
+        }
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+            SaveFile();
         }
 
         private void MenuItem_Click_5(object sender, RoutedEventArgs e)
@@ -412,6 +422,16 @@ namespace BookShelf
             var current = new AuthorOverview(AuthorGrid.SelectedValue);
             var window = new PaginalView(authorLoader, current);
             window.ShowDialog();
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void CommandBinding_Executed_1(object sender, ExecutedRoutedEventArgs e)
+        {
+            LoadFile();
         }
     }
 }
